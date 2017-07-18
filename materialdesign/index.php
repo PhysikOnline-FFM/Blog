af628dd21ff135b41e4691322515459036d203c8
Thomas Lai <lai.thomas@outlook.de>
2017-07-18 22:50:23 +0200

    v0

---
+++ materialdesign/index.php
@@ -0,0 +1,97 @@
+<?php 
+get_header();
+?>
+
+<!--NAVBAR-->
+<nav id="nav" class="light-blue darken-4 z-depth-3">
+    <div class="containernav">
+        <div class="nav-wrapper">
+        <a href="<?php bloginfo('template_url'); ?>/index.php" class="brand-logo white-text">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">   
+            <li><a class="white-text" href="<?php bloginfo('template_url'); ?>/page.php">Projekte</a></li>
+            <li><a class="white-text" href="badges.html">Über Uns</a></li>
+            <li><a class="white-text" href="collapsible.html">Kontakt</a></li>
+        </ul>
+        </div>
+    </div>
+</nav>    
+
+<!--TITELBILD-->
+<div class="parallax-container">
+    <div class="parallax">
+        <img src="<?php bloginfo('template_url')?>/assets/images/desk.jpg">
+    </div>
+    <div id="parent" class="black trans overlay">
+    <div id="headline" class="caption center-align">
+      <h3 class="white-text light">PhysikOnline Blog</h3>
+      <h5 class="light grey-text text-lighten-3">Alles rund um unsere Projekte, wer wir sind und was wir vorhaben.</h5>
+    </div>
+    </div>
+</div>
+
+
+ <!--MAIN-->       
+<div class="section blue-grey lighten-5">
+<br><br>
+<div class="divider"></div>
+    <div class="row container">
+    <br>    <br>
+    <div class="row">
+     
+    <div id="Main" class="col m9">
+    
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>  
+      <div class="card horizontal" >
+        <div class="card-image">
+         <img id="card-img" src="
+          <?php if ( has_post_thumbnail() )
+            echo  wp_get_attachment_url( get_post_thumbnail_id($post->ID) )
+            ?>
+            ">
+        </div>
+        <div class="card-stacked" >
+        <div class="card-content">
+            <h4 class="thin"> 
+            <?php the_title(); ?>
+            </h4>
+          <span class="thin">
+          <?php the_content(); ?>
+          <br><br>
+          <a href="<?php the_permalink()?>"> mehr.. </a>
+          </span>
+        </div>
+        
+        </div>
+      </div>
+        
+    <?php endwhile; endif; ?>  
+      
+    </div>
+     <!--SIDEBAR-->
+     <?php get_sidebar() ?>
+     
+    </div>        
+</div>
+
+</div>
+</div>
+
+
+
+<!-- FOOTER -->
+      
+ <footer id="Footer" class="page-footer light-blue darken-4">
+      <div class="footer-copyright">
+        <div class="container">
+            © 2017 PhysikOnline
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <ul>
+            <li><a href="#">Impressum</a></li>
+            <li><a href="#">Datenschutz</a></li>
+            <li><a href="#">Kontakt</a></li>
+            </ul>
+        </div>
+      </div>
+</footer>        
+
+<?php get_footer(); ?>
---
+++ materialdesign/single.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/materialize.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css">
+
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>PhysikOnline Blog</title>
+
+</head>
+
+<body
+<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+         echo ' style="background: url('. $url.'); " '; ?> 
+>
+<!-- NAVBAR -->    
+<div id="Header">
+<nav class="light-blue darken-4" >
+    <div class="container">
+        <div class="nav-wrapper">
+        <a href="../index.php" class="brand-logo white-text">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a class="white-text" href="sass.html">Projekte</a></li>
+            <li><a class="white-text" href="badges.html">Über Uns</a></li>
+            <li><a class="white-text" href="collapsible.html">Kontakt</a></li>
+        </ul>
+        </div>
+    </div>
+</nav>
+</div>
+
+<?php the_post(); ?>
+
+
+
+
+<div id="parent" class="white trans">
+
+              
+      <div class="extrapadding" >
+          <div class="container">
+              <div class="card">
+                  <div class="article">
+                     <h3 class="thin"><?php the_title(); ?></h3>
+                      <span class="light"> 
+                      <?php the_content(); ?> 
+                      </span>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      
+</div>
+
+
+
+
+
+
+
+ <footer id="Footer" class="page-footer light-blue darken-4">
+      <div class="footer-copyright">
+        <div class="container">
+            © 2017 PhysikOnline
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <ul>
+            <li><a href="#">Impressum</a></li>
+            <li><a href="#">Datenschutz</a></li>
+            <li><a href="#">Kontakt</a></li>
+            </ul>
+        </div>
+      </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/ThatsNotYoChild.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/materialize.js"></script>
+<script>$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+</script>
+
+
+</body>
+
+
+</html>
